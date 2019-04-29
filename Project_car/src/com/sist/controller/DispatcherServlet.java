20aaadaec0da31dcbd6d04a023579ca673e130f6
ki <krs1231@naver.com>
2019-04-29 17:11:32 +0900

    mvc 수정

--- Project_car/src/com/sist/controller/DispatcherServlet.java
+++ Project_car/src/com/sist/controller/DispatcherServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
-@WebServlet("/DispatcherServlet")
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	List<String> clsList = new ArrayList<String>();
