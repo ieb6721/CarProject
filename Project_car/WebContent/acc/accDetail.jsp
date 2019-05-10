fca1a9b516d8f46ab298e282fcbd783d7861849d
ldm <ldm@DESKTOP-M71ICGS>
2019-05-10 15:47:08 +0900

    acc

--- Project_car/WebContent/acc/accDetail.jsp
+++ Project_car/WebContent/acc/accDetail.jsp
@@ -321,19 +321,22 @@ function change () {
 				</div>
 				<div class="row">
 					<div class="product__wrap clearfix">
+					
+					<c:forEach var="vo" items="${list }">
 						<!-- Start Single Product -->
 						<div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
 							<div class="category">
 								<div class="ht__cat__thumb">
-									<a href="product-details.html"> <img
-										src="../images/accessory_img/product/1.jpg"
-										alt="product images">
+									<a href="product-details.html"> 
+								<img src="${vo.product_main_img}" alt="product images">
 									</a>
 								</div>
 								<div class="fr__hover__info">
 									<ul class="product__action">
-										<li><a href="wishlist.html"><i
-												class="icon-heart icons"></i></a></li>
+										<li>
+										<a href="#" class="btn btn-xs btn-info">X</a>
+										<!-- <a href="wishlist.html"> -->
+										</a></li>
 
 										<li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
 
@@ -342,112 +345,19 @@ function change () {
 								</div>
 								<div class="fr__product__inner">
 									<h4>
-										<a href="product-details.html">Product Title Here </a>
+										<a href="product-details.html">${vo.product_name }</a>
 									</h4>
 									<ul class="fr__pro__prize">
-										<li class="old__prize">$30.3</li>
-										<li>$25.9</li>
+										<li class="old__prize">${vo.product_origin_price }</li>
+										<li>${vo.product_dc_price }원</li>
 									</ul>
 								</div>
 							</div>
 						</div>
+						</c:forEach>
 						<!-- End Single Product -->
-						<!-- Start Single Product -->
-						<div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-							<div class="category">
-								<div class="ht__cat__thumb">
-									<a href="product-details.html"> <img
-										src="../images/accessory_img/product/2.jpg"
-										alt="product images">
-									</a>
-								</div>
-								<div class="fr__hover__info">
-									<ul class="product__action">
-										<li><a href="wishlist.html"><i
-												class="icon-heart icons"></i></a></li>
-
-										<li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
-
-										<li><a href="#"><i class="icon-shuffle icons"></i></a></li>
-									</ul>
-								</div>
-								<div class="fr__product__inner">
-									<h4>
-										<a href="product-details.html">Product Title Here </a>
-									</h4>
-									<ul class="fr__pro__prize">
-										<li class="old__prize">$30.3</li>
-										<li>$25.9</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<!-- End Single Product -->
-
-
-
-						<!-- Start Single Product -->
-						<div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-							<div class="category">
-								<div class="ht__cat__thumb">
-									<a href="product-details.html"> <img
-										src="../images/accessory_img/product/3.jpg"
-										alt="product images">
-									</a>
-								</div>
-								<div class="fr__hover__info">
-									<ul class="product__action">
-										<li><a href="wishlist.html"><i
-												class="icon-heart icons"></i></a></li>
-
-										<li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
-
-										<li><a href="#"><i class="icon-shuffle icons"></i></a></li>
-									</ul>
-								</div>
-								<div class="fr__product__inner">
-									<h4>
-										<a href="product-details.html">Product Title Here </a>
-									</h4>
-									<ul class="fr__pro__prize">
-										<li class="old__prize">$30.3</li>
-										<li>$25.9</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<!-- End Single Product -->
-						<!-- Start Single Product -->
-						<div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-							<div class="category">
-								<div class="ht__cat__thumb">
-									<a href="product-details.html"> <img
-										src="../images/accessory_img/product/4.jpg"
-										alt="product images">
-									</a>
-								</div>
-								<div class="fr__hover__info">
-									<ul class="product__action">
-										<li><a href="wishlist.html"><i
-												class="icon-heart icons"></i></a></li>
-
-										<li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
-
-										<li><a href="#"><i class="icon-shuffle icons"></i></a></li>
-									</ul>
-								</div>
-								<div class="fr__product__inner">
-									<h4>
-										<a href="product-details.html">Product Title Here </a>
-									</h4>
-									<ul class="fr__pro__prize">
-										<li class="old__prize">$30.3</li>
-										<li>$25.9</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<!-- End Single Product -->
+						
+					
 					</div>
 				</div>
 			</div>
--- Project_car/src/com/sist/model/AccModel.java
+++ Project_car/src/com/sist/model/AccModel.java
@@ -1,170 +1,121 @@
 package com.sist.model;
 
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.sist.controller.RequestMapping;
 import com.sist.dao.*;
 
 import com.sist.vo.*;
 
-
 public class AccModel {
 
-/*	@RequestMapping("acc/acc.do")
-	public String accModel(HttpServletRequest request) {
-		
-		List<AccVO> list=AccDAO.accListData();
-		request.setAttribute("aList", list);
-		//System.out.println(list.size());
-		return "../acc/acc.jsp";
-	}*/
-/*	@RequestMapping("acc/acc.do")  
-	public String accModel(HttpServletRequest request) {
-		
-		List<AccVO> list=AccDAO.accListData1(map);
-		request.setAttribute("aList", list);
-		//System.out.println(list.size());
-		return "../acc/acc.jsp";
-	}*/
+
 	@RequestMapping("acc/accDetail.do")
-	public String acc_detail(HttpServletRequest request) 
-	{
+	public String acc_detail(HttpServletRequest request ,HttpServletResponse response) {
+
+		String product_id = request.getParameter("product_id");
+
+		AccVO vo = AccDAO.accDetailData(product_id);
+		Cookie cookie=new Cookie("product_id"+product_id,product_id);
+		cookie.setMaxAge(60*60*24);
+		response.addCookie(cookie);
 		
-		String product_id=request.getParameter("product_id");
+		Cookie[] cookies = request.getCookies();
 		
-		AccVO vo=AccDAO.accDetailData(product_id);
+		List<AccVO> list=new ArrayList<AccVO>();
 		
+
+		for (int i = 0; i < cookies.length; i++) {
+			if (cookies[i].getName().startsWith("product_id")) {
+				String value = cookies[i].getValue();
+				AccVO rvo = AccDAO.accDetailData(value);
+				list.add(rvo);
+			}
+
+		}
+	
+		request.setAttribute("list", list);
 		
-		//String price=vo.setProduct_dc_price(product_dc_price);
-				
+
 		request.setAttribute("vo", vo);
 		request.setAttribute("acc_jsp", "../acc/accDetail.jsp");
-		
+
 		return "../acc/accDetail.jsp";
 	}
-	
-	
+
 	@RequestMapping("acc/acc.do")
-	  public String acc_list(HttpServletRequest request)
-	  {
-		    String[] cateList = { "전체","시트", "거치대", "가리개", "방향제", "목쿠션", "핸들커버", "먼지털이" };
-		    String strPage=request.getParameter("page");
-		    String no=request.getParameter("no");
-		    if(strPage==null)
-		    	strPage="1";
-		    if(no == null)
-		    	no="0";
-		   
-		
-		    int curpage=Integer.parseInt(strPage);
-		    int rowSize=9;
-		    int start=(curpage*rowSize)-(rowSize-1);
-		    int end=(curpage*rowSize);
-		    Map map=new HashMap();
-		    map.put("start",start);
-		    map.put("end",end);
-		    map.put("cate", cateList[Integer.parseInt(no)]);
-		    List<AccVO> alist=AccDAO.accListData(map);
-		    int totalpage=AccDAO.accTotalPage(map);
-		    
-		    final int BLOCK=5;
-		    int allPage=totalpage;
-		    int startPage=((curpage-1)/BLOCK*BLOCK)+1;
-		    /*
-		       curpage : 1~5 ==> BLOCK 1
-		          (6-1)/5*5 => 5+1 => 6
-		          
-		       현재 페이지 ==> 1~5 : 1
-		            ==> 6~10 : 6
-		            ==> 11~15 : 11
-		    */
-		    int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
-		    if(endPage>allPage)
-		    	endPage=allPage;
-		    request.setAttribute("no", no);
-		    request.setAttribute("alist", alist);
-		    request.setAttribute("curpage", curpage);
-		    request.setAttribute("totalpage", totalpage);
-		    request.setAttribute("BLOCK", BLOCK);
-		    request.setAttribute("startPage", startPage);
-		    request.setAttribute("endPage", endPage);
-		    request.setAttribute("allPage", allPage);
-		    
-		    request.setAttribute("accList_jsp", "acc_list.jsp");
-		
-		    
-			 
-			  //System.out.println(no);
-			  
-			  
-			 /* if(no!=null)
-			  {
-				  String cate=cateList[Integer.parseInt(no)];
-				   List<AccVO> list=AccDAO.accCate(cate);
-				   
-				   request.setAttribute("list", list);
-			  }*/
-			
-			  
-		    
-		/*    String[] cateList = { "시트", "거치대", "가리개", "방향제", "목쿠션", "핸들커버", "먼지털이" };
-		    String no=request.getParameter("no");
-		    String cate=null;
-			   if(no!=null)
-			     cate=cateList[Integer.parseInt(no)];
-			   Map map1=new HashMap();
-			   map1.put("cate", cate);
-			   List<AccVO> list=AccDAO.accCate(map1);
-			   request.setAttribute("list", list);*/
-			  
-		    
-		  return "../acc/acc.jsp";
-	  }
-	
+	public String acc_list(HttpServletRequest request) {
+		String[] cateList = { "전체", "시트", "거치대", "가리개", "방향제", "목쿠션", "핸들커버", "먼지털이" };
+		String strPage = request.getParameter("page");
+		String no = request.getParameter("no");
+		if (strPage == null)
+			strPage = "1";
+		if (no == null)
+			no = "0";
+
+		int curpage = Integer.parseInt(strPage);
+		int rowSize = 9;
+		int start = (curpage * rowSize) - (rowSize - 1);
+		int end = (curpage * rowSize);
+		Map map = new HashMap();
+		map.put("start", start);
+		map.put("end", end);
+		map.put("cate", cateList[Integer.parseInt(no)]);
+		List<AccVO> alist = AccDAO.accListData(map);
+		int totalpage = AccDAO.accTotalPage(map);
+
+		final int BLOCK = 5;
+		int allPage = totalpage;
+		int startPage = ((curpage - 1) / BLOCK * BLOCK) + 1;
+		/*
+		 * curpage : 1~5 ==> BLOCK 1 (6-1)/5*5 => 5+1 => 6
+		 * 
+		 * 현재 페이지 ==> 1~5 : 1 ==> 6~10 : 6 ==> 11~15 : 11
+		 */
+		int endPage = ((curpage - 1) / BLOCK * BLOCK) + BLOCK;
+		if (endPage > allPage)
+			endPage = allPage;
+		request.setAttribute("no", no);
+		request.setAttribute("alist", alist);
+		request.setAttribute("curpage", curpage);
+		request.setAttribute("totalpage", totalpage);
+		request.setAttribute("BLOCK", BLOCK);
+		request.setAttribute("startPage", startPage);
+		request.setAttribute("endPage", endPage);
+		request.setAttribute("allPage", allPage);
+
+		request.setAttribute("accList_jsp", "acc_list.jsp");
+
+
+		return "../acc/acc.jsp";
+	}
+
 	@RequestMapping("acc/acc_search.do")
-	public String acc_search(HttpServletRequest request)
-	{
-		try
-		{
-			//한글 변환
+	public String acc_search(HttpServletRequest request) {
+		try {
+			// 한글 변환
 			request.setCharacterEncoding("UTF-8");
-			//컴파일 예외처리 => 반드시 컴파일 전에 예외처리를 한다
-		}catch(Exception ex){}
-		
-		String keyword=request.getParameter("keyword");
-		
-		List<AccVO> list=AccDAO.accSearchData(keyword);
-		
+			// 컴파일 예외처리 => 반드시 컴파일 전에 예외처리를 한다
+		} catch (Exception ex) {
+		}
+
+		String keyword = request.getParameter("keyword");
+
+		List<AccVO> list = AccDAO.accSearchData(keyword);
+
 		System.out.println(list.size());
-		
-		request.setAttribute("alist", list);	
-				
+
+		request.setAttribute("alist", list);
+
 		return "acc_list.jsp";
 	}
-	
-/*	@RequestMapping("acc/acc.do")
-	  public String acc_cookie(HttpServletRequest request)
-	  {
 
-		Cookie[] cookies=request.getCookies();
-		for(int i=0;i<cookies.length;i++)
-		{
-			if(cookies[i].getName().startsWith("product_id"))
-			{
-				String value=cookies[i].getValue();
-			}
-			
-		}
-		
-		
-		return "../acc/acc.jsp";
-		
-	  }*/
-	 
-	 
+
 }
